b4f10578821bf3284d713d23e213c25546798d3d
Dhanielgeek <dhanielknightz46@gmail.com>
2026-01-16 00:16:00 +0100

    fix the chatbox..

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,20 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _smartsupp = _smartsupp || {};
+              _smartsupp.key = '19e3432c82dd6e61fcb0baf9e1d80df783cdc935';
+              window.smartsupp||(function(d) {
+                var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+                s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+                c.type='text/javascript';c.charset='utf-8';c.async=true;
+                c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+              })(document);
+            `,
+          }}
+        />
       </Head>
 
       <body className="antialiased">
