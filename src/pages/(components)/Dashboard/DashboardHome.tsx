4cb6f9825fe3003b62d1adaaddb632806e62263a
Dhanielgeek <dhanielknightz46@gmail.com>
2026-02-06 00:26:07 +0100

    updated the new feature...

--- src/pages/(components)/Dashboard/DashboardHome.tsx
+++ src/pages/(components)/Dashboard/DashboardHome.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Portfolio_Home from './Portfolio_Home'
+import React from "react";
+import Navbar from "./Navbar";
+import Portfolio_Home from "./Portfolio_Home";
+import { useAuthGuard } from "@/hooks/useAuthguards";
 
 const DashboardHome = () => {
+  useAuthGuard();
   return (
-    <div className='w-full h-full flex flex-col justify-between'>
-      <div className=''>
-        <Portfolio_Home/>
+    <div className="w-full h-full flex flex-col justify-between">
+      <div className="">
+        <Portfolio_Home />
       </div>
-      <div className='w-full bg-white pt-3 px-15 pb-5 z-40'>
-        <Navbar/>
+      <div className="w-full bg-white pt-3 px-15 pb-5 z-40">
+        <Navbar />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardHome
+export default DashboardHome;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,24 @@
-export const emailRegex =
-  /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+import { jwtDecode } from "jwt-decode";
+
+export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
 
 export const passwordRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^()_+=])[A-Za-z\d@$!%*?&#^()_+=]{8,}$/;
 
-export const validateEmail = (email: string) =>
-  emailRegex.test(email);
+export const validateEmail = (email: string) => emailRegex.test(email);
 
 export const validatePassword = (password: string) =>
   passwordRegex.test(password);
+
+interface DecodedToken {
+  exp: number;
+}
+
+export const isTokenExpired = (token: string) => {
+  try {
+    const { exp } = jwtDecode<DecodedToken>(token);
+    return Date.now() >= exp * 1000;
+  } catch {
+    return true;
+  }
+};
